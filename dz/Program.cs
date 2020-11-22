1de48b34f66606306f37501950799158a71fdd54
Technik <fobos1100@yandex.ru>
2020-11-22 15:56:43 +0300

    Добавьте файлы проекта.

---
+++ dz/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace dz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "";
+            string s2;
+            bool res = false;
+            s1 = Console.ReadLine();
+            s2 = s1;
+            s1 = s1.ToLower();
+            s1.Replace("!", "");
+            s1.Replace(",", "");
+            s1.Replace(".", "");
+            s1.Replace("-", "");
+            s1.Replace(" ", "");
+            for (int i = 0, j = s1.Length - 1; i < j; i++, j--)
+            {
+                if (s1[i] == s1[j])
+                {
+                    res = true;
+                }
+                else
+                {
+                    res = false;
+                    break;
+                }
+            }
+            if (res == true)
+            {
+                Console.WriteLine($"{s2} is palindrom");
+            }
+            else
+            {
+                Console.WriteLine($"{s2} is not palindrom");
+            }
+
+        }
+    }
+}
